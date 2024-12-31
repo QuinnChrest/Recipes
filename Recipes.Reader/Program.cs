eab544b1f93cc4f3f2d7fff93c2c65b60d1ed3a8
Quinn Chrest <quinnchr@gmail.com>
2024-12-30 21:22:50 -0600

    Read text from image using tesseract and then have gpt convert it to a markdown format.

--- Recipes.Reader/Program.cs
+++ Recipes.Reader/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OpenAI.Chat;
+using Tesseract;
+
+try
+{
+    var base_folder = Environment.GetEnvironmentVariable("BASE_FOLDER") ?? "C:\\Deploy\\Recipes";
+    var output_folder = Path.Combine(base_folder, "OUTPUT");
+    Directory.CreateDirectory(output_folder);
+
+    var openai_api_key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
+    if (string.IsNullOrEmpty(openai_api_key))
+    {
+        Console.WriteLine("Invalid OpenAI API key provided.");
+        return;
+    }
+
+    var files = Directory.GetFiles(base_folder);
+
+    foreach (var file in files)
+    {
+        Console.WriteLine($"Processing: {file}");
+
+        if (string.IsNullOrEmpty(file) || !File.Exists(file))
+        {
+            Console.WriteLine($"No file was found at: {file}");
+            return;
+        }
+
+        using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+        using (var img = Pix.LoadFromFile(file))
+        using (var page = engine.Process(img))
+        {
+            var text = page.GetText();
+            var client = new ChatClient("gpt-4o-mini", openai_api_key);
+            var completion = client.CompleteChat($"This is a recipe that I have scanned in, could you return it back to me with markdown formatting. I only want the recipe int he result you return me because I will ingest that data: \n {text}");
+            File.WriteAllText(Path.Combine(output_folder, $"{Path.GetFileNameWithoutExtension(file)}.txt"), completion.Value.Content[0].Text);
+        }
+    }
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
